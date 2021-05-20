92a3e5908ab2ad3ea8b7358e56d9f50bbe21a3a0
gh175775 <gh175775@alibaba-inc.com>
2021-05-20 14:32:09 +0800

    slice

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -6,11 +6,18 @@
 // storage and a size.  The user of a Slice must ensure that the slice
 // is not used after the corresponding external storage has been
 // deallocated.
+
+// slice是一种指向外部存储的数据结构, 其本身只存储指针
+// 使用slice一个非常重要的点在于确保外部存储内存块没有被free掉
+// 如果学过golang应该非常好理解 slice在go中几乎是亲儿子
 //
 // Multiple threads can invoke const methods on a Slice without
 // external synchronization, but if any of the threads may call a
 // non-const method, all threads accessing the same Slice must use
 // external synchronization.
+
+// const方法在被多线程调用的时候不需要额外的并发控制
+// 但是如果访问非const方法, 那么所有访问相应slice的线程都需要进行并发控制
 
 #ifndef STORAGE_LEVELDB_INCLUDE_SLICE_H_
 #define STORAGE_LEVELDB_INCLUDE_SLICE_H_
@@ -26,19 +33,25 @@ namespace leveldb {
 
 class LEVELDB_EXPORT Slice {
  public:
+  // 几种创建slice的方式
   // Create an empty slice.
+  // 空的
   Slice() : data_(""), size_(0) {}
 
   // Create a slice that refers to d[0,n-1].
+  // 创建指向指定内存块的
   Slice(const char* d, size_t n) : data_(d), size_(n) {}
 
   // Create a slice that refers to the contents of "s"
+  // 创建指向c++字符串的
   Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
 
   // Create a slice that refers to s[0,strlen(s)-1]
+  // 创建指向c语言字符串的
   Slice(const char* s) : data_(s), size_(strlen(s)) {}
 
   // Intentionally copyable.
+  // 设置复制构造函数
   Slice(const Slice&) = default;
   Slice& operator=(const Slice&) = default;
 
@@ -53,6 +66,7 @@ class LEVELDB_EXPORT Slice {
 
   // Return the ith byte in the referenced data.
   // REQUIRES: n < size()
+  // []操作符重载 返回n位置的字符
   char operator[](size_t n) const {
     assert(n < size());
     return data_[n];
@@ -65,6 +79,7 @@ class LEVELDB_EXPORT Slice {
   }
 
   // Drop the first "n" bytes from this slice.
+  // slice便宜n位指针
   void remove_prefix(size_t n) {
     assert(n <= size());
     data_ += n;
@@ -72,6 +87,7 @@ class LEVELDB_EXPORT Slice {
   }
 
   // Return a string that contains the copy of the referenced data.
+  // 返回c++格式字符串
   std::string ToString() const { return std::string(data_, size_); }
 
   // Three-way comparison.  Returns value:
@@ -81,15 +97,19 @@ class LEVELDB_EXPORT Slice {
   int compare(const Slice& b) const;
 
   // Return true iff "x" is a prefix of "*this"
+  // 判断某个slice是否以传入的slice为前缀
   bool starts_with(const Slice& x) const {
     return ((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
   }
 
  private:
+  // slice的起始地址
   const char* data_;
+  // slice的大小
   size_t size_;
 };
 
+// == 操作符重载操作
 inline bool operator==(const Slice& x, const Slice& y) {
   return ((x.size() == y.size()) &&
           (memcmp(x.data(), y.data(), x.size()) == 0));
@@ -98,7 +118,12 @@ inline bool operator==(const Slice& x, const Slice& y) {
 inline bool operator!=(const Slice& x, const Slice& y) { return !(x == y); }
 
 inline int Slice::compare(const Slice& b) const {
+  // 取短的slice的长度为比较长度
   const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
+  // memcmp比较函数
+  // 如果data_ < b.data_ 返回-1
+  // 如果data_ == b.data_ 返回0
+  // 如果data_ > b.data_ 返回1
   int r = memcmp(data_, b.data_, min_len);
   if (r == 0) {
     if (size_ < b.size_)
